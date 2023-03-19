3124b0d52df0d13120814ae20761bf6d2c03b554
Abdullah Omar <fishilike.fish@gmail.com>
2023-03-20 04:21:21 +0600

    Create jwt.strategy.ts

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secret-but-need-to-be-secure',
+    });
+  }
+
+  async validate(payload: { sub: number; user: User }) {
+    return { userId: payload.sub, user: payload.user };
+  }
+}
